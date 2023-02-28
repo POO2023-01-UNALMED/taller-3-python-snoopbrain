237a26d8be5e2d1693c715935a82946f5fa8f324
snoopbrain <87624067+snoopbrain@users.noreply.github.com>
2023-02-28 10:42:12 -0500

    Zd

--- televisores/marca.py
+++ televisores/marca.py
@@ -1,10 +1,9 @@
 
-class Marca:
-    def __init__(self, nombre):
-        self._nombre = nombre
-    
-    def getNombre(self):
-        return self._nombre
-
-    def setNombre(self, nombre):
-        self._nombre = nombre
+class Marca: 
+     def __init__(self, nombre): 
+         self._nombre=nombre 
+     def setNombre(self, nombre): 
+         self._nombre=nombre 
+  
+     def getNombre(self): 
+         return self._nombre
